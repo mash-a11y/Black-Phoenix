1a18c7d58bf15701c732aa665ba5e96054d99da6
mash-a11y <mashoodhassan09@gmail.com>
2026-05-23 00:19:52 +0500

    Update login.php

--- public/login.php
+++ public/login.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enter the Matrix</title>
+    <title>Black Phoenix</title>
     <link rel="stylesheet" href="../assets/css/login.css">  <!-- Link to the CSS file for styling -->
 </head>
 <body>
